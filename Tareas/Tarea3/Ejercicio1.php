ebc8569dfb1a1050f5b5029b4d3a7dfc944a5611
Grover101 <limachigrover09@gmail.com>
2021-04-12 12:01:33 -0400

    Tarea 3 Finalizada

--- Tareas/Tarea3/Ejercicio1.php
+++ Tareas/Tarea3/Ejercicio1.php
@@ -11,27 +11,29 @@
 
 <body>
     <h1>Ejercicio 1</h1>
-    <form action="./Ejercicio1.php" method="GET">
-        <label for="entrada">Introduzca el numero a generar la serie fibonacci:</label>
-        <br>
-        <input type="number" name="n" id="entrada">
-        <button type="submit" name="enviar">Generar la serie</button>
-    </form>
-    <div>
-        <ol>
-            <?php
-            if (isset($_GET['enviar'])) {
-                $n = $_GET['n'];
-                $a = 0;
-                $b = 1;
-                for ($i = 0; $i < $n; $i++) {
-                    $a += $b;
-                    $b = $a - $b;
-                    echo '<li>' . $a . '</li>';
+    <div class="contenido">
+        <a href="./index.html" class="volver">Volver</a>
+        <form action="./Ejercicio1.php" method="GET">
+            <label for="entrada">Introduzca el numero a generar la serie fibonacci:</label>
+            <input type="number" name="n" id="entrada" required>
+            <button type="submit" name="enviar">Generar la serie</button>
+        </form>
+        <div class="contenido">
+            <ol>
+                <?php
+                if (isset($_GET['enviar'])) {
+                    $n = $_GET['n'];
+                    $a = 0;
+                    $b = 1;
+                    for ($i = 0; $i < $n; $i++) {
+                        $a += $b;
+                        $b = $a - $b;
+                        echo '<li>' . $a . '</li>';
+                    }
                 }
-            }
-            ?>
-        </ol>
+                ?>
+            </ol>
+        </div>
     </div>
 </body>
 
--- Tareas/Tarea3/Ejercicio2.php
+++ Tareas/Tarea3/Ejercicio2.php
@@ -11,33 +11,31 @@
 
 <body>
     <h1>Ejercicio 2</h1>
-    <form action="./Ejercicio2.php" method="GET">
-        <label for="i">Introduzca el numero i (Filas):</label>
-        <br>
-        <input type="number" name="i" id="i">
-        <br>
-        <label for="j">Introduzca el numero j (Columnas):</label>
-        <br>
-        <input type="number" name="j" id="j">
-        <br>
-        <button type="submit" name="enviar">Construir tabla</button>
-    </form>
-    <div>
-        <table border="1">
-
-            <?php
-            if (isset($_GET['enviar'])) {
-                $i = $_GET['i'];
-                $j = $_GET['j'];
-                for ($n = 0; $n < $i; $n++) {
-                    echo "<tr>";
-                    for ($m = 0; $m < $j; $m++)
-                        echo "<td><pre> </pre></td>";
-                    echo "</tr>";
+    <div class="contenido">
+        <a href="./index.html" class="volver">Volver</a>
+        <form action="./Ejercicio2.php" method="GET">
+            <label for="i">Introduzca el numero i (Filas):</label>
+            <input type="number" name="i" id="i" required>
+            <label for="j">Introduzca el numero j (Columnas):</label>
+            <input type="number" name="j" id="j" required>
+            <button type="submit" name="enviar">Construir tabla</button>
+        </form>
+        <div>
+            <table>
+                <?php
+                if (isset($_GET['enviar'])) {
+                    $i = $_GET['i'];
+                    $j = $_GET['j'];
+                    for ($n = 0; $n < $i; $n++) {
+                        echo "<tr>";
+                        for ($m = 0; $m < $j; $m++)
+                            echo "<td><pre> </pre></td>";
+                        echo "</tr>";
+                    }
                 }
-            }
-            ?>
-        </table>
+                ?>
+            </table>
+        </div>
     </div>
 </body>
 

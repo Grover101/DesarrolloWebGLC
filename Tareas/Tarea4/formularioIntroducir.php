5ab189a1e3c4fd6ddf6fdca540d0d90fd16db844
Grover101 <limachigrover09@gmail.com>
2021-04-21 23:03:57 -0400

    Tarea 4 Finalizada

--- Tareas/Tarea4/formularioIntroducir.php
+++ Tareas/Tarea4/formularioIntroducir.php
@@ -15,23 +15,29 @@ if (isset($_GET['n'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulario</title>
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 
 <body>
+    <h1>Llenar vectores</h1>
     <form action="./sumar.php" method="GET">
         <label for="">Vector 1: </label>
-        <?php
-        for ($i = 0; $i < $n; $i++)
-            echo "<input type='number' name='a[$i]'>";
-        ?>
+        <div class="contenido">
+            <?php
+            for ($i = 0; $i < $n; $i++)
+                echo "<input type='number' name='a[$i]'>";
+            ?>
+        </div>
         <br>
         <label for="">Vector 2: </label>
-        <?php
-        for ($i = 0; $i < $n; $i++)
-            echo "<input type='number' name='b[$i]'>";
-        ?>
+        <div class="contenido">
+            <?php
+            for ($i = 0; $i < $n; $i++)
+                echo "<input type='number' name='b[$i]'>";
+            ?>
+        </div>
         <br>
-        <button type="submit">Calular la Suma</button>
+        <button type="submit" name="calcular">Calular la Suma</button>
     </form>
 </body>
 
--- Tareas/Tarea4/sumar.php
+++ Tareas/Tarea4/sumar.php
@@ -1,14 +1,3 @@
-<?php
-
-if (isset($_COOKIE['tam'])) {
-    $n = $_COOKIE['tam'];
-    $vector1 = $_GET['a'];
-    $vector2 = $_GET['b'];
-
-    for ($i = 0; $i < $n; $i++)
-        $vector1[$i] += $vector2[$i];
-}
-?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -17,14 +6,25 @@ if (isset($_COOKIE['tam'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Resultado</title>
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 
 <body>
-    <table border="1">
+    <h1>Resultado de la suma de dos vetores</h1>
+    <table>
         <tr>
             <?php
-            for ($i = 0; $i < $n; $i++)
-                echo "<td>" . $vector1[$i] . "</td>";
+            if (isset($_COOKIE['tam']) && isset($_GET['calcular'])) {
+                $n = $_COOKIE['tam'];
+                $vector1 = $_GET['a'];
+                $vector2 = $_GET['b'];
+
+                for ($i = 0; $i < $n; $i++)
+                    $vector1[$i] += $vector2[$i];
+
+                for ($i = 0; $i < $n; $i++)
+                    echo "<td>" . $vector1[$i] . "</td>";
+            }
             ?>
         </tr>
     </table>

0dfc34b6d32ca9b2909c420b0ce4d179a91cef2f
Grover101 <limachigrover09@gmail.com>
2021-04-27 02:19:27 -0400

    Tarea 5 Finalizada

--- Tareas/Tarea5/Ejercicio2/tablaintercalado.php
+++ Tareas/Tarea5/Ejercicio2/tablaintercalado.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="refresh" content="4;URL=./formulario.html">
+    <!-- <meta http-equiv="refresh" content="6;URL=./formulario.html"> -->
     <title>Imprimir</title>
     <link rel="stylesheet" href="../css/style.css">
 </head>
@@ -18,15 +18,18 @@
                 $fila = $_POST['fila'];
                 $columna = $_POST['columna'];
                 $i = 1;
+                $j = 1;
                 for ($n = 0; $n < $fila; $n++) {
                     echo "<tr>";
                     for ($m = 0; $m < $columna; $m++) {
                         if ($m % 2 == 0)
                             echo "<td class='bolivia$i'>&nbsp</td>";
                         else
-                            echo "<td class='chuquisaca$i'>&nbsp</td>";
+                            echo "<td class='chuquisaca$j'>&nbsp</td>";
                     }
                     if ($i == 3) $i = 0;
+                    if ($j == 2) $j = 0;
+                    $j++;
                     $i++;
                     echo "</tr>";
                 }

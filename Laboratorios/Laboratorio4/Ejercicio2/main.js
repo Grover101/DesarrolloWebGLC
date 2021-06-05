23da57ab2a654b85dcce5bc117a52093dd891303
Grover101 <limachigrover09@gmail.com>
2021-06-05 10:17:18 -0400

    Ejercicio 2 terminado

---
+++ Laboratorios/Laboratorio4/Ejercicio2/main.js
@@ -0,0 +1,12 @@
+const seleccion = (n) => {
+    let factorial = 1;
+    let opcion = ''
+    for (let i = 1; i <= n; i++) {
+        factorial *= i
+        opcion = opcion + `<option value="${i}">${factorial}</option>`
+    }
+    document.getElementById('seccion').innerHTML = `<h1>Ejercicio 2</h1><span>Factorial de un Numero: </span><select>${opcion}</select>`
+}
+
+var numero = parseInt(prompt('Inserte un N factorial'))
+seleccion(numero)

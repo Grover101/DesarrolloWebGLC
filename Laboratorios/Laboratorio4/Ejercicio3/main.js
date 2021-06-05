73d641b3427239825666f43b6e14eba404d48135
Grover101 <limachigrover09@gmail.com>
2021-06-05 11:47:14 -0400

    Ejercicio 3 terminado

---
+++ Laboratorios/Laboratorio4/Ejercicio3/main.js
@@ -0,0 +1,5 @@
+cambioColor = (color) => {
+    // let color = document.getElementsByName('color');
+    document.getElementById('contenedor').className = color;
+    document.getElementById('contenedor').innerHTML = "<span>Grover T. Limachi Carrillo</spar>"
+}

4cb7ddf6f1e8f661d232c10d5efa86a451cc5f09
Grover101 <limachigrover09@gmail.com>
2021-06-08 01:16:41 -0400

    Tarea 8 Finalizada

--- Tareas/Tarea8/main.js
+++ Tareas/Tarea8/main.js
@@ -3,6 +3,17 @@ calcular = () => {
     var b = parseFloat(document.getElementById('b').value);
     var c = parseFloat(document.getElementById('c').value);
 
-    document.getElementById('resultado1').innerHTML = `<p>El resultado para x1 es: ${(-b + Math.sqrt(b * b - 4 * a * c)) / (2 * a)}</p>`;
-    document.getElementById('resultado2').innerHTML = `<p>El resultado para x2 es: ${(-b - Math.sqrt(b * b - 4 * a * c)) / (2 * a)}</p>`;
+    var d = b * b - 4 * a * c;
+
+    if (d == 0) {
+        document.getElementById('resultado1').innerHTML = `<p>El resultado para x1 es: ${-b / (2 * a)}</p>`;
+        document.getElementById('resultado2').innerHTML = `<p>El resultado para x2 es: ${-b / (2 * a)}</p>`;
+    } else if (d > 0) {
+        document.getElementById('resultado1').innerHTML = `<p>El resultado para x1 es: ${(-b + Math.sqrt(d)) / (2 * a)}</p>`;
+        document.getElementById('resultado2').innerHTML = `<p>El resultado para x2 es: ${(-b - Math.sqrt(d)) / (2 * a)}</p>`;
+    } else {
+        console.log(d, -b / (a * 2), Math.sqrt(-d));
+        document.getElementById('resultado1').innerHTML = `<p>El resultado para x1 es: ${-b / (2 * a)} + ${Math.sqrt(-d) / (2 * a)}i</p>`;
+        document.getElementById('resultado2').innerHTML = `<p>El resultado para x2 es: ${-b / (2 * a)} - ${Math.sqrt(-d) / (2 * a)}i</p>`;
+    }
 }
